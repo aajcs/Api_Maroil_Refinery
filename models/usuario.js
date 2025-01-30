9581125b9b05dc9ecab00533d5a3ab87588cf1b0
aajcs <98472648+aajcs@users.noreply.github.com>
2025-01-30 14:27:56 -0400

    quitando version de usuarios

--- models/usuario.js
+++ models/usuario.js
@@ -1,59 +1,58 @@
+const { Schema, model } = require("mongoose");
 
-const { Schema, model } = require('mongoose');
-
-const UsuarioSchema = Schema({
+const UsuarioSchema = Schema(
+  {
     nombre: {
-        type: String,
-        required: [true, 'El nombre es obligatorio']
+      type: String,
+      required: [true, "El nombre es obligatorio"],
     },
     correo: {
-        type: String,
-        required: [true, 'El correo es obligatorio'],
-        unique: true
+      type: String,
+      required: [true, "El correo es obligatorio"],
+      unique: true,
     },
     password: {
-        type: String,
-        required: [true, 'La contraseña es obligatoria'],
+      type: String,
+      required: [true, "La contraseña es obligatoria"],
     },
-    
+
     rol: {
-        type: String,
-        required: true,
-        default: 'USER_ROLE',
-        enum: ['ADMIN_ROLE', 'USER_ROLE']
+      type: String,
+      required: true,
+      default: "USER_ROLE",
+      enum: ["ADMIN_ROLE", "USER_ROLE"],
     },
     estado: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
-   /* google: {
+    /* google: {
         type: Boolean,
         default: false
     },*/
     acceso: {
-        type: String,
-        required: true,
-        default: 'ACCESS_LIMIT',
-        enum: ['ACCESS_LIMIT', 'FULL_ACCESS']
+      type: String,
+      required: true,
+      default: "ACCESS_LIMIT",
+      enum: ["ACCESS_LIMIT", "FULL_ACCESS"],
     },
-    id_refineria: [{
+    id_refineria: [
+      {
         type: Schema.Types.ObjectId,
-        ref: 'Refineria',            
-    }],
-
-},
-{
+        ref: "Refineria",
+      },
+    ],
+  },
+  {
     timestamps: true,
-    versionKey: false
+    versionKey: false,
   }
 );
 
+UsuarioSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...usuario } = this.toObject();
+  usuario.id = _id;
+  return usuario;
+};
 
-
-UsuarioSchema.methods.toJSON = function() {
-    const { password, _id, ...usuario  } = this.toObject();
-    usuario.id = _id;
-    return usuario;
-}
-
-module.exports = model( 'Usuario', UsuarioSchema );
+module.exports = model("Usuario", UsuarioSchema);
