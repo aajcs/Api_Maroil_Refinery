63ccea9c3242e7dcbe539dd6bc35fa57f0e4bedd
Ivanbrac <ivanbrac.ib@gmail.com>
2025-05-14 09:29:20 -0400

    recepcionBK

---
+++ models/bunkering/recepcionBK.js
@@ -0,0 +1,213 @@
+const { Schema, model } = require("mongoose");
+const auditPlugin = require("./plugins/audit");
+const Counter = require("./counter");
+
+// Subesquema para Tractomula
+const TractomulaSchema = new Schema(
+  {
+    idGuia: {
+      type: Number,
+      required: [true, "El ID de la Guía es obligatorio"],
+    },
+    placa: {
+      type: String,
+      maxlength: [10, "La placa no puede exceder los 10 caracteres"],
+      required: [true, "La placa es obligatoria"],
+    },
+    nombreChofer: {
+      type: String,
+      maxlength: [
+        20,
+        "El nombre del chofer no puede exceder los 50 caracteres",
+      ],
+    },
+  },
+  { _id: false }
+);
+
+// Subesquema para Refinería
+const RefineriaSchema = new Schema(
+  {
+    idRefineria: {
+      type: Schema.Types.ObjectId,
+      ref: "Refineria",
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+// Subesquema para Bunkering
+const BuqueSchema = new Schema(
+  {
+    datosBuque: {
+      type: Object, // Puedes definir aquí los campos específicos si lo deseas
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+// Definición del esquema para el modelo Recepción
+const RecepcionBKSchema = new Schema(
+  {
+    // Número de despacho
+    numeroRecepcion: {
+      type: Number,
+    },
+    // Relación con el modelo Contrato
+    idContrato: {
+      type: Schema.Types.ObjectId,
+      ref: "ContratoBK",
+      required: [
+        true,
+        "El ID del Contrato asociado a la recepción es obligatorio",
+      ],
+    },
+    idContratoItems: {
+      type: Schema.Types.ObjectId,
+      ref: "ContratoItemsBK",
+    },
+    idLinea: {
+      type: Schema.Types.ObjectId,
+      ref: "LineaCargaBK",
+    },
+    idBunkering: {
+      type: Schema.Types.ObjectId,
+      ref: "Bunkering",
+      required: [
+        true,
+        "El ID del Bunkering asociada a la recepción es obligatorio",
+      ],
+    },
+    idTanque: {
+      type: Schema.Types.ObjectId,
+      ref: "TanqueBK",
+    },
+    idChequeoCalidad: {
+      type: Schema.Types.ObjectId,
+      ref: "ChequeoCalidadBK",
+      required: false,
+    },
+    idChequeoCantidad: {
+      type: Schema.Types.ObjectId,
+      ref: "ChequeoCantidadBK",
+      required: false,
+    },
+    cantidadRecibida: {
+      type: Number,
+      min: [0, "La cantidad recibida no puede ser negativa"],
+    },
+    cantidadEnviada: {
+      type: Number,
+      min: [0, "La cantidad enviada no puede ser negativa"],
+      required: [true, "La cantidad enviada es obligatoria"],
+    },
+    estadoRecepcion: {
+      type: String,
+    },
+    estadoCarga: {
+      type: String,
+    },
+    estado: {
+      type: String,
+    },
+    fechaInicio: {
+      type: Date,
+    },
+    fechaFin: {
+      type: Date,
+    },
+    fechaDespacho: {
+      type: Date,
+    },
+    fechaInicioRecepcion: {
+      type: Date,
+    },
+    fechaFinRecepcion: {
+      type: Date,
+    },
+    fechaSalida: {
+      type: Date,
+    },
+    fechaLlegada: {
+      type: Date,
+    },
+    
+
+    // NUEVO CAMPO: Tipo de recepción y su estructura asociada
+    tipo: {
+      type: String,
+      required: [true, "El tipo de recepción es obligatorio"],
+      enum: ["Tractomula", "Refineria", "Bunkering"],
+    },
+    tractomula: {
+      type: TractomulaSchema,
+      required: function () {
+        return this.tipo === "Tractomula";
+      },
+      default: undefined,
+    },
+    refineria: {
+      type: RefineriaSchema,
+      required: function () {
+        return this.tipo === "Refineria";
+      },
+      default: undefined,
+    },
+    bunkering: {
+      type: BuqueSchema,
+      required: function () {
+        return this.tipo === "Bunkering";
+      },
+      default: undefined,
+    },
+
+    // Control de estado (eliminación lógica)
+    eliminado: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+RecepcionBKSchema.plugin(auditPlugin);
+
+RecepcionBKSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+// Middleware para generar un número único de refinación
+RecepcionBKSchema.pre("save", async function (next) {
+  if (this.isNew && this.idBunkering) {
+    try {
+      const counterKey = `recepcion_${this.idBunkering.toString()}`;
+      let bunkeringCounter = await Counter.findOne({ _id: counterKey });
+
+      if (!bunkeringCounter) {
+        bunkeringCounter = new Counter({ _id: counterKey, seq: 999 });
+        await bunkeringCounter.save();
+      }
+
+      bunkeringCounter.seq += 1;
+      await bunkeringCounter.save();
+
+      this.numeroRecepcion = bunkeringCounter.seq;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
+});
+
+module.exports = model("RecepcionBK", RecepcionBKSchema);
