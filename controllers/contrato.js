95a70a3336430b21eb43694b054ed03d0b4867ca
José Capelo <jcapelos04@gmail.com>
2025-06-27 16:42:54 -0400

    ahora si

--- controllers/contrato.js
+++ controllers/contrato.js
@@ -619,7 +619,7 @@ const contratoDelete = async (req, res = response, next) => {
         eliminado: false,
         $or: [
           { acceso: "completo" },
-          { acceso: "limitado", idRefineria: nuevoContrato.idRefineria._id },
+          { acceso: "limitado", idRefineria: nContrato.idRefineria._id },
         ],
       });
 
